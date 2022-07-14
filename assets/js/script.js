46dd47a3d258ac3f1555c89136113f560b09274b
Kelly Wacker <kelwacker@gmail.com>
2022-07-13 20:42:21 -0400

    future weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,17 @@ var cityNameEl = document.querySelector(".city-name");
 var searchBtn = document.querySelector(".search-here");
 // temp in box
 var currentWeatherEl = document.querySelector("#current-weather");
+//entire futre forecast container
+var futureContainerEl = document.querySelector(".forecast");
+// future card container
+var futureCardsEl = document.querySelector("#forecast");
+// forecast title 
+var forecastTitleEl = document.querySelector(".forecast-title");
+// search history
+var searchHistoryEl = document.querySelector("#search-history");
+
+var searchHistoryBtn = document.querySelector(".search-history-btn");
+
 var cities = [];
 
 var formSearchWeather = function(event) {
@@ -18,13 +29,12 @@ var formSearchWeather = function(event) {
     var location = formInputEl.value.trim();
     if (location) {
       getWeather(location);
-      getFutureWeather(location);
-      cities.unshift({location});
       formInputEl.value = "";
     } else {
       alert("Enter a location");
     }
-    saveSearch();
+    saveLocation(location);
+    searchHistory(location);
 };
 
 var getWeather = function(location) {
@@ -44,10 +54,6 @@ var getWeather = function(location) {
     .catch(function(error){
         alert("Not connected");
     });
-};
-
-var saveSearch = function(){
-    localStorage.setItem("cities", JSON.stringify(cities));
 };
 
 var displayWeather = function(weather, searchTerm) {
@@ -87,13 +93,13 @@ var displayWeather = function(weather, searchTerm) {
 };
 
 var getUVIndex = function(latitude, longitude) {
-    var newApiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=2ea6c593bf0230a2e5e91e4e3927cc96";
-    fetch(newApiURL).then(function(response) {
+    var apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=2ea6c593bf0230a2e5e91e4e3927cc96";
+    fetch(apiURL).then(function(response) {
         response.json().then(function(data){
             showUVIndex(data);
-            // console.log(data);
         });
     });
+    getFutureWeather(latitude, longitude);
 };
 
 var showUVIndex = function (uvi) {
@@ -119,6 +125,79 @@ var showUVIndex = function (uvi) {
     currentWeatherEl.appendChild(uvIndexTitle);
 };
 
-var getFutureWeather = function(location) {};
+var getFutureWeather = function(latitude, longitude) {
+    var apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=2ea6c593bf0230a2e5e91e4e3927cc96";
+    fetch(apiURL).then(function(response) {
+        response.json().then(function(data){
+            console.log(data);
+            displayFutureWeather(data);
+        });
+    });
+};
+
+var displayFutureWeather = function(weather) {
+    futureCardsEl.textContent = "";
+    forecastTitleEl.textContent = "5 Day Forecast:";
+
+    // sets dailyWeather equal to the array inside weather that holds the forecast for week
+    let i = 1;
+    var dailyWeather = weather.daily[i];
+    // loops through the dailyWeather array
+    while (i <= 5) {
+        // creates html element, adds class, and appends to main container
+        var forecastCard = document.createElement("div");
+        forecastCard.classList = "col-2 card future";
+        futureCardsEl.appendChild(forecastCard);
+
+        // date
+        var dateEl = document.createElement("h2");
+        dateEl.textContent = moment.unix(weather.daily[i].dt).format("MMM DD, YYYY");
+        forecastCard.append(dateEl);
+
+        // icon
+        var iconEl = document.createElement("img");
+        var iconKey = weather.daily[i].weather[0].icon;
+        iconURL = "https://openweathermap.org/img/w/" + iconKey + ".png";
+        iconEl.setAttribute("src", iconURL);
+        forecastCard.append(iconEl);
+
+        // temperature
+        var tempEl = document.createElement("span");
+        tempEl.textContent = "Temp: " + weather.daily[i].feels_like.day + " °F";
+        forecastCard.appendChild(tempEl);
+        // wind speed
+        var windEl = document.createElement("span");
+        windEl.textContent = "Wind: " + weather.daily[i].wind_speed + " mph";
+        forecastCard.appendChild(windEl);
+        // humidity
+        var humidityEl = document.createElement("span");
+        humidityEl.textContent = "Humidity: " + weather.daily[i].humidity + " %";
+        forecastCard.appendChild(humidityEl);
+
+        i++
+    };
+};
+
+// var saveSearch = function() {
+//     localStorage.setItem("City", JSON.stringify(cities));
+// }
+
+var searches = [];
+var saveLocation = function(location) {
+    localStorage.setItem("City", JSON.stringify(location));
+    searches.push()
+}
+
+var searchHistory = function(searched) {
+    var searchHistory = document.createElement("button");
+    searchHistory.textContent = searched;
+    searchHistoryEl.appendChild(searchHistory);
+    searchHistory.classList = "btn city-name search-history-btn";
+};
+
+var retrieveHistory = function (location) {
+
+};
 
 searchBtn.addEventListener("click", formSearchWeather);
+// searchHistoryBtn.addEventListener("click", retrieveHistory);
